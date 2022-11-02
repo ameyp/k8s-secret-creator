82faa609423f7baa793b52f80b62e7aca8b99ed5
Amey Parulekar <amey@tetra.ai>
2022-11-01 22:53:18 -0700

    Prettify test debugging output.

--- test.sh
+++ test.sh
@@ -12,14 +12,18 @@ while [ $i -lt 10 ] # Attempt a max of 10 times
 do
     secret_exists=$(kubectl get secret mostest-secret -n secret-namespace)
     if [[ $? == 0 ]]; then
+        echo "Secret found, exiting."
         exit 0
     fi
 
+    echo "Retrying."
     sleep 1
     ((i++))
 done
 
-echo "Secret was not created. The following secrets exist: "
+echo "Secret was not created."
+echo "--- Existing secrets ---"
 kubectl get secret -n secret-namespace
+echo "--- Pod logs -----------"
 kubectl logs local-test -n secret-namespace
 exit 1
